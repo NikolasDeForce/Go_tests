e8873ad234ac9dc0bffe71200a951f242e13bdbf
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-10 23:39:17 +0700

    last

--- select/racer.go
+++ select/racer.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
+var tenSecondTimeout = 10 * time.Second
 
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
 
-	if aDuration < bDuration {
-		return a
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
+}
 
-	return b
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
