72787d68a0f459735d58e0ea7b31a270db6bcba5
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-13 19:51:04 +0700

    last

--- sync/sync.go
+++ sync/sync.go
@@ -1,10 +1,15 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestSync(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -9,11 +12,29 @@ func TestSync(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		assertCounter(t, counter, 3)
+		assertCounter(t, &counter, 3)
+	})
+
+	t.Run("it run safely concurrency", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, &counter, wantedCount)
 	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 
 	if got.Value() != want {
