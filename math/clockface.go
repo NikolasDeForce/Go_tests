66d2dec453a478a921239cc192f6f5afc154228e
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-18 17:48:23 +0700

    last

--- math/clockface.go
+++ math/clockface.go
@@ -10,8 +10,16 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCentreX = 150
+const clockCentreY = 150
+
 func SecondHand(t time.Time) Point {
-	return Point{150, 90}
+	p := secondsHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
+	return p
 }
 
 func secondsInRadians(t time.Time) float64 {
--- math/clockface_test.go
+++ math/clockface_test.go
@@ -48,6 +48,17 @@ func TestSecondsInPoint(t *testing.T) {
 	}
 }
 
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v wanted %v", got, want)
+	}
+}
+
 func roughlyEqualFloat64(a, b float64) bool {
 	const equalityThreshold = 1e-7
 	return math.Abs(a-b) < equalityThreshold
