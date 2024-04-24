035d6ffc6611ab29b256e80d2a6f12f5d9aaea04
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-24 17:38:47 +0700

    html templates renderer

---
+++ blogrenderer/blockrenderer_test.go
@@ -0,0 +1,34 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"testing"
+	"tests/blogrenderer"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it convert a single post to HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrenderer.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+}
