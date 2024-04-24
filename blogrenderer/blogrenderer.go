3bbf0c0c9f95e26d9d7fc20d26e5f0961e7ddd32
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-24 17:48:30 +0700

    html templates renderer

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,30 @@
+package blogrenderer
+
+import (
+	"html/template"
+	"io"
+)
+
+type Post struct {
+	Title       string
+	Body        string
+	Description string
+	Tags        []string
+}
+
+const (
+	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+)
+
+func Render(w io.Writer, p Post) error {
+	templ, err := template.New("blog").Parse(postTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
