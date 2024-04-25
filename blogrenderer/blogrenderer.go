e722cd34ebfc7b2b69725d8a6cf3f73c951cf41d
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-25 17:49:03 +0700

    html templates renderer

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -6,18 +6,27 @@ import (
 	"io"
 )
 
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplate, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
 var (
 	//go:embed "templates/*"
 	postTemplate embed.FS
 )
 
-func Render(w io.Writer, p Post) error {
-	templ, err := template.ParseFS(postTemplate, "templates/*.gohtml")
-	if err != nil {
-		return err
-	}
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
 
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
 		return err
 	}
 
--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 	"tests/blogrenderer"
 
@@ -18,18 +19,39 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRender, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it convert a single post to HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRender.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRender, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRender.Render(io.Discard, aPost)
+	}
 }
