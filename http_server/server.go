76be5366a2400127f6460ca88c708978bd9ab535
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-29 15:12:06 +0700

    http-server tests v1

--- http_server/server.go
+++ http_server/server.go
@@ -1,10 +1,22 @@
-package httpserver
+package main
 
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+	if name == "Floyd" {
+		return "10"
+	}
+	return ""
 }
--- http_server/server_test.go
+++ http_server/server_test.go
@@ -1,4 +1,4 @@
-package httpserver
+package main
 
 import (
 	"net/http"
@@ -15,6 +15,19 @@ func TestGETPlayers(t *testing.T) {
 
 		got := response.Body.String()
 		want := "20"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("return Floyd's scores", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
