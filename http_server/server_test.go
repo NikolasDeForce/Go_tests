377e7c496c14d45dd8c320946f685520f9b50454
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-29 16:56:29 +0700

    http-server tests v2 - refactoring funcs, test -- all tests passed

--- http_server/server_test.go
+++ http_server/server_test.go
@@ -8,7 +8,8 @@ import (
 )
 
 type StubPlayersStore struct {
-	scores map[string]int
+	scores   map[string]int
+	winCalls []string
 }
 
 func TestGETPlayers(t *testing.T) {
@@ -17,6 +18,7 @@ func TestGETPlayers(t *testing.T) {
 			"Pepper": 20,
 			"Floyd":  10,
 		},
+		nil,
 	}
 
 	server := &PlayerServer{&store}
@@ -53,16 +55,27 @@ func TestGETPlayers(t *testing.T) {
 func TestStoreWins(t *testing.T) {
 	store := StubPlayersStore{
 		map[string]int{},
+		nil,
 	}
 	server := &PlayerServer{&store}
 
 	t.Run("it returns accepted on POST", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		player := "Pepper"
+
+		req := newPostWinRequest(player)
 		res := httptest.NewRecorder()
 
 		server.ServeHTTP(res, req)
 
 		assertStatusCode(t, res.Code, http.StatusAccepted)
+
+		if len(store.winCalls) != 1 {
+			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not correct winner got %q want %q", store.winCalls[0], player)
+		}
 	})
 }
 
@@ -71,8 +84,17 @@ func (s *StubPlayersStore) GetPlayerScore(name string) int {
 	return score
 }
 
+func (s *StubPlayersStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
 func NewGetScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func newPostWinRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
 	return req
 }
 
