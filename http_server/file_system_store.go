05a8f5e03bf296d9033f48e9ff5a648dd3e15f98
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-30 11:32:43 +0700

    http-server tests v4 - add new test json reader

---
+++ http_server/file_system_store.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type FileSystemPlayerStore struct {
+	database io.Reader
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	var league []Player
+	json.NewDecoder(f.database).Decode(&league)
+	return league
+}
---
+++ http_server/file_system_store_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestFileSystemStore(t *testing.T) {
+	t.Run("league from a reader", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetLeague()
+
+		want := []Player{
+			{"Cleo", 10},
+			{"Chris", 33},
+		}
+
+		assertLeague(t, got, want)
+	})
+}
