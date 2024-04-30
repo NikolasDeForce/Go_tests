5847351a28ad037f37372d2c712a8e6ad6655734
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-04-30 11:47:57 +0700

    http-server tests v4 - add new test to parse json -  player score

--- http_server/file_system_store.go
+++ http_server/file_system_store.go
@@ -13,3 +13,16 @@ func (f *FileSystemPlayerStore) GetLeague() []Player {
 	league, _ := NewLeague(f.database)
 	return league
 }
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
+}
--- http_server/file_system_store_test.go
+++ http_server/file_system_store_test.go
@@ -22,4 +22,20 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Chris")
+
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
