bc3635699ba140b0f9ee26c9e94e524d670fb6cb
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-05-02 00:26:11 +0700

    http-server tests v6 -- add func and test

---
+++ http_server/cli_test.go
@@ -0,0 +1,13 @@
+package poker
+
+import "testing"
+
+func TestCLI(t *testing.T) {
+	playerStore := StubPlayersStore{}
+	cli := &CLI{playerStore}
+	cli.PlayPoker()
+
+	if len(playerStore.winCalls) != 1 {
+		t.Fatal("expected a win call but didn't get any")
+	}
+}
