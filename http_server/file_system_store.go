0460e990c8b6579bff916c0fde6ac1df0f63e374
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-05-01 13:25:41 +0700

    http-server tests v5 - refactoring func

--- http_server/file_system_store.go
+++ http_server/file_system_store.go
@@ -13,17 +13,10 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, io.SeekStart)
-
-	info, err := file.Stat()
+	err := initialisePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file into from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, io.SeekStart)
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -62,4 +55,21 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	}
 
 	f.database.Encode(f.league)
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, io.SeekStart)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file into from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, io.SeekStart)
+	}
+
+	return nil
 }
