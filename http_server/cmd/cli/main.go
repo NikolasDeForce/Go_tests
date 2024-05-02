8f588f39199d5322909fa14eeacd415d6640735b
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-05-02 09:23:55 +0700

    http-server tests v6 - refactoring main.go

--- http_server/cmd/cli/main.go
+++ http_server/cmd/cli/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	poker "tests/http_server"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
 	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v ", err)
+	}
+
+	game := poker.CLI{store, os.Stdin}
+	game.PlayPoker()
 }
