2039b64bf2c44a3669bb86c5c4e9f201f2f5722f
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-05-03 10:51:20 +0700

    http-server tests v7 - refactoring funcs

--- http_server/cli.go
+++ http_server/cli.go
@@ -26,22 +26,25 @@ func NewCli(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.scheduleBlindAlerts()
+	userInput := cli.readLine()
+	cli.PlayerStore.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) scheduleBlindAlerts() {
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
 	blindTime := 0 * time.Second
 	for _, blind := range blinds {
 		cli.Alerter.ScheduledAlertAt(blindTime, blind)
 		blindTime = blindTime + 10*time.Minute
 	}
-
-	userInput := cli.readLine()
-	cli.PlayerStore.RecordWin(extractWinner(userInput))
-}
-
-func extractWinner(userInput string) string {
-	return strings.Replace(userInput, " wins", "", 1)
 }
 
 func (cli *CLI) readLine() string {
 	cli.In.Scan()
 	return cli.In.Text()
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
 }
